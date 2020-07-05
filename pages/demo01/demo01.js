eff9f32fc514f5dcf72b08228fca95203bdae8af
leetao <673416736@qq.com>
2020-07-05 15:03:58 +0800

    wechat base-js study conpleted

--- pages/demo01/demo01.js
+++ pages/demo01/demo01.js
@@ -1,66 +1,33 @@
-// pages/demo01/demo01.js
+//Page Object
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    "message": "test",
+    "num": 1000,
+    "boolean":true,
+    "person":{
+      "id":1000000,
+      "name":"lisa"
+    },
+    list:[
+      {
+        id:"111",
+        name:"lisa"
+      },
+      {
+        id:"2222",
+        name:"jack"
+      }
+    ],
+    newNum: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleInput(e){
+    this.setData({
+      newNum: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleTap(e){
+    let operator = e.target.dataset.operation
+    console.log(operator)
   }
-})
+});
+  
